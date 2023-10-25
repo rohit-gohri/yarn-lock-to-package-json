ea241279d61ce6f7ab20ccd5488314df19a88d8d
Rohit Gohri <code@rohit.page>
2023-10-25 14:40:14 +0200

    better output

--- scripts/test.mjs
+++ scripts/test.mjs
@@ -9,9 +9,10 @@
 
 /// <reference types="zx/globals" />
 
-const { verbose, test: only, clean = true } = argv;
+const { verbose = false, test: only, clean = true } = argv;
 
-$.verbose = !!verbose;
+$.verbose = false;
+process.env.FORCE_COLOR=3
 
 const all = [
   "bin",
@@ -28,23 +29,49 @@ const all = [
 
 const tests = all.filter((test) => !only || test === only);
 
+/**
+ * 
+ * @param {Awaited<ReturnType<$>>} output
+ * @param {string} prefix 
+ */
+const printWithPrefix = (output, prefix) => {
+  console.log(output.stdout.split("\n").map((line) => {
+    return `${prefix} ${line}`;
+  }).join("\n"));
+  if (output.stderr) {
+    console.log(output.stderr.split("\n").map((line) => {
+      return `${prefix} ${line}`;
+    }).join("\n"));
+  }
+}
+
 const results = (
   await Promise.allSettled(
-    tests.map(
-      (test) =>
-        $`cd tests/${test} && \
-    yarn > /dev/null && \
-    echo "" && \
-    echo "Generating package.json from lockfile" && \
-    echo "" && \
-    node ${__dirname}/../cli.js && \
-    echo "" && \
-    echo "Updating lock file based on generated package.json" && \
-    echo "" && \
-    yarn && \
-    git diff --exit-code yarn.lock
-`
-    )
+    tests.map(async (test) => {
+      const prefix = chalk.yellowBright(`[${test}]: `);
+
+      await $`cd tests/${test} && yarn > /dev/null`;
+      console.log(prefix, chalk.blueBright('Generating package.json from lockfile.'))
+
+      const generate = await $`cd tests/${test} && node ${__dirname}/../cli.js`;
+      if (verbose) {
+        printWithPrefix(generate, prefix);
+      }
+
+      console.log(prefix, chalk.blueBright('Updating lock file based on generated package.json.'))
+
+      const yarn = await $`cd tests/${test} && yarn`;
+      if (verbose) {
+        printWithPrefix(yarn, prefix);
+      }
+
+      const diff = await $`cd tests/${test} && git diff --exit-code yarn.lock`;
+      if (verbose) {
+        printWithPrefix(diff, prefix);
+      }
+
+      console.log(prefix, chalk.greenBright('No diff detected'))
+    })
   )
 ).map((res, index) => {
   return {
@@ -61,7 +88,11 @@ const failures = results.filter((result) => result.status === "rejected");
 
 const success = results.filter((result) => result.status === "fulfilled");
 
-console.log(`${success.length}/${all.length} tests passed, ${failures.length}/${all.length} tests failed, ${all.length - results.length}/${all.length} tests skipped`);
+console.log(
+  `${success.length}/${all.length} tests passed, ${failures.length}/${
+    all.length
+  } tests failed, ${all.length - results.length}/${all.length} tests skipped`
+);
 
 if (results.length === 0) {
   throw new Error("No tests to run");
