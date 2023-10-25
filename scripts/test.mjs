4c77c8b13c278ff77e1d80e5d15bbaa728a07bd2
Rohit Gohri <code@rohit.page>
2023-10-25 02:34:04 +0200

    add tests script

---
+++ scripts/test.mjs
@@ -0,0 +1,48 @@
+#!/usr/bin/env zx
+/* global argv, fs, $ */
+
+/**
+ * This script is to be used using https://github.com/google/zx
+ *
+ * Use with local zx via `yarn zx`
+ */
+
+/// <reference types="zx/globals" />
+
+$.verbose = false;
+
+const tests = [
+  "bin",
+  "patch",
+  "patch-monorepo",
+  "patch-resolution",
+  "resolutions",
+  "scoped",
+  "alias-subdeps",
+  "portal",
+  "link",
+  "same-resolution",
+];
+
+const results = await Promise.allSettled(
+  tests.map(
+    (test) =>
+      $`cd tests/${test} && \
+    yarn && \
+    node ${__dirname}/../cli.js && \
+    yarn && \
+    git diff --exit-code yarn.lock
+`
+  )
+);
+
+await $`git checkout tests`;
+
+const failures = results.filter((result) => result.status === "rejected");
+
+if (failures.length > 0) {
+  console.log(JSON.stringify(failures, null, 2));
+  throw new Error("Some tests failed");
+}
+
+process.exit(0);
